4776eff65f035536c8aa7ffb6585433f0cf5ee89
howlil <muhammadulilabshar27@gmail.com>
2024-12-24 01:23:45 +0700

    update deploy

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,14 +1,9 @@
 const admin = require('firebase-admin');
-require('dotenv').config();
 
-// Path ke file service account Firebase Anda
 const serviceAccount = require('../../service-accounts.json');
-const serviceAccount2 = JSON.parse(process.env.FIREBASE_KEY);
-
-const credentials = process.env.NODE_ENV === 'production' ? serviceAccount2 : serviceAccount;
 
 admin.initializeApp({
-  credential: admin.credential.cert(credentials),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 module.exports = admin;
